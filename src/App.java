8b1f5eb9e8cff2098b47fca34f820450e30d85c8
MyllsYumYum <20180013279@my.xu.edu.ph>
2024-03-13 13:58:57 +0800

    first commit

---
+++ src/App.java
@@ -0,0 +1,18 @@
+//in the bengigging
+import java.sql.*;
+import java.util.ArrayList;
+
+abstract class services{
+    int[] serviceNames = new int[10];
+    String[] serviceCosts = new String[10];
+    
+    int partyTricks;
+    int balloonShapes;
+
+}
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
