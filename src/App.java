2bf63a69b4ccdf12baf729540c6acfca292c6938
MyllsYumYum <20180013279@my.xu.edu.ph>
2024-03-15 13:18:27 +0800

    Update 3

--- src/App.java
+++ src/App.java
@@ -53,19 +53,140 @@ class services{
 
 class user extends services{
     String name = "John Doe"; //default name
-    int userID = 666; //default user id
+    int userID = 0; //default user id
     user(){ //constructor
-
+        System.out.println(name); //test stuff
+        System.out.println(userID); //test stuff
     }
 
 }
 
-public class App {
-    Scanner sc = new Scanner(System.in);
-    user testUser = new user();
-    App(){
-        testUser.add();
+class client{
+    int clientID = 0;
+    ArrayList<Integer> invoiceKeys = new ArrayList<Integer>();
 
+}
+class invoice{
+    int invoiceID = 0;
+    int userKey = 0;
+    boolean[] serviceKeys = new boolean[10];
+    ArrayList<Integer> clientKeys = new ArrayList<Integer>();
+}
+
+public class App {
+    int currentUser = 0;
+    void menu(int menum){
+        switch (menum) {
+            case 0: //Main Menu
+            System.out.println();
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n Current User : " + userList[currentUser].name);
+                System.out.println("1. ) Change User \n2. ) Access Services \n3. ) Access Clients \n4. ) Access Invoices \n 9. ) Exit Program");
+                menu(sc.nextInt());
+                break;
+        
+            case 1: //Change user
+            System.out.println();
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n Choose User");
+                System.out.println("0. ) Add User");
+                for (int index = 0; index < 5; index++) {
+                    if (userList[index] == null) {
+                        break;
+                    }
+                    System.out.println(index+1 + ". ) " + userList[index].name );
+                }
+                switch (sc.nextInt()) {
+                        case 0: //Adds new user
+                            if (userList[1] == null) {
+                                System.err.println("User slot empty, Please select or add a valid user");
+                                menu(1);
+                                break;
+                            }
+                            break;
+
+                        case 1: //user 1
+                            if (userList[1] == null) {
+                                System.err.println("User slot empty, Please select or add a valid user");
+                                menu(1);
+                                break;
+                            }
+                            break;
+                        case 2: //user 2
+                            if (userList[2] == null) {
+                                System.err.println("User slot empty, Please select or add a valid user");
+                                menu(1);
+                                break;
+                            }
+                            break;
+                            
+                        case 3: //user 3
+                            if (userList[3] == null) {
+                                System.err.println("User slot empty, Please select or add a valid user");
+                                menu(1);
+                                break;
+                            }
+                            break;
+    
+                        case 4: //user 4
+                            if (userList[4] == null) {
+                                System.err.println("User slot empty, Please select or add a valid user");
+                                menu(3);
+                                break;
+                            }
+                            break;
+                        case 5: //user 5
+                            if (userList[5] == null) {
+                                System.err.println("User slot empty, Please select or add a valid user");
+                                menu(1);
+                                break;
+                            }
+                            break;
+                    
+                        default:
+                            menu(0);
+                            break;
+                }
+            case 2: //Access services
+            System.out.println();
+                    
+            case 3: //Access client
+            System.out.println();
+                switch (sc.nextInt()) {
+                    case 1: //add client
+                    
+                    case 2: //delete client
+                        
+                    case 3: //access client records
+
+                    case 4: //return to main menu
+                        menu(0);
+                        break;
+                
+                    default:
+                        menu(0);
+                        break;
+                }
+                
+            case 4: //Access invoices
+            System.out.println();
+                
+            
+            case 9: //Exit program
+            System.out.println();
+            System.err.println("Closing Command Line Process");
+            System.exit(1);
+            default:
+                menu(0);
+                break;
+        }
+    }
+    Scanner sc = new Scanner(System.in);
+    user[] userList = new user[5];
+    
+    
+    App(){
+        userList[0] = new user();
+        menu(0);
+        //
     }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
